a76d07613f50b9338d3a1dd4211b1b66cfb78f10
Jochyd <mail>
2022-10-30 21:38:59 -0300

    agregamos los middlewares morgan y static

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 //crear una instancia de express es crear una variable app
 
@@ -11,4 +14,31 @@ app.listen( app.get('port'), ()=>{
     console.log('estoy en el puerto '+ app.get('port'))
 })
 
-console.log('hola mundo')
+//midlewares: funciones que se ejecutan antes de las rutas
+//cors es para recibir peticiones permite conexiones remotas.
+app.use(cors());
+
+//de express son los siguientes
+//permiten interpretar el formato json cuando alguien haga una peticion
+app.use(express.json());
+app.use(express.urlencoded({extends:true}));
+app.use(morgan('dev'));
+
+// cargar un archivo estatico
+app.use(express.static(path.join(__dirname, '../public')))
+//aqui es la ruta donde esta ubicado 
+// console.log(path.join(__dirname, '../public'));
+
+
+//dotenv para tomar variables de entorno
+
+//morgan nos da informacion en la terminal 
+
+//rutas
+// http://localhost:4000/prueba
+app.get('/prueba', (req, res)=>{
+    res.send('esto es una prueba de una peticion get')
+})
+app.delete('/prueba', (req, res)=>{
+    res.send('aqui tendria que borrar un producto o dato')
+})
