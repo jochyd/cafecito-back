2802ca001c5b3194fb17e8eed242a1f7608c3c41
Jochyd <mail>
2022-11-02 21:57:37 -0300

    agregue las rutas

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -51,7 +51,36 @@ router
 router
   .route("/productos/:id")
   .get(obtenerProductos)
-  .put(editarProducto)
+  .put( 
+    [
+    check("nombreProducto")
+      .notEmpty()
+      .withMessage("El nombre del producto es un dato obligatorio")
+      .isLength({min: 2, max: 100})
+      .withMessage('El nombre del producto debe tener entre 2 y 100 caracteres'),
+      check('precio')
+      .notEmpty()
+      .withMessage('El precio es un dato obligatorio')
+      .isNumeric()
+      .withMessage('El precio debe ser un numero')
+      .custom((value)=>{
+          if(value >= 1 && value <=10000){
+              return true
+          }else{
+              throw new Error('El precio debe estar entre 1 y 10000')
+          }
+      }),
+      check('imagen')
+      .notEmpty()
+      .withMessage('La url de la imagen es obligatoria')
+      .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+      .withMessage('Debe ingresar una URL valida'),
+      check('categoria')
+      .notEmpty()
+      .withMessage('La categoria es un dato obligatorio')
+      .isIn(['Bebida Caliente','Bebida Fría', 'Dulce', 'Salado'])
+      .withMessage('Debe ingresar una categoria valida'),
+  ],editarProducto)
   .delete(borrarProducto);
 
 // app.get('/prueba', (req, res)=>{
